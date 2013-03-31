fd213d9e308225825a62dc64ddd541f360919f82
Lucas Brendel <lucasbrendel@gmail.com>
2013-03-31 18:57:19 -0500

    Refactored user profile functions

--- SharpFit/Resources/User/FitBitUser.cs
+++ SharpFit/Resources/User/FitBitUser.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using SharpFit.Resources.User;
+using SharpFit.OAuth;
 
 namespace SharpFit.Resources.User
 {
@@ -18,5 +22,70 @@ namespace SharpFit.Resources.User
         /// </summary>
         [JsonProperty("user")]
         public Profile User { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void GetUserInfo()
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest("/user/-/profile.json");
+            client.ExecuteAsync(request, response =>
+            {
+                FitBitUser p = new FitBitUser();
+                p = JsonConvert.DeserializeObject<FitBitUser>(response.Content);
+                NotifyProfileComplete(p);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public delegate void GetProfileHandler(object sender, ProfileEventArgs e);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event GetProfileHandler GetProfileCompleted;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        public void UpdateUserInfo(Dictionary<string, string> param)
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest("/user/-/profile.json", Method.POST);
+            foreach (KeyValuePair<string, string> p in param)
+            {
+                request.AddParameter(p.Key, p.Value);
+            }
+            client.ExecuteAsync(request, response =>
+            {
+                FitBitUser prof = new FitBitUser();
+                prof = JsonConvert.DeserializeObject<FitBitUser>(response.Content);
+                NotifyProfileComplete(prof);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="profile"></param>
+        private void NotifyProfileComplete(FitBitUser profile)
+        {
+            if (GetProfileCompleted != null)
+            {
+                GetProfileCompleted(this, new ProfileEventArgs(profile));
+            }
+        } 
     }
+
+
 }
--- SharpFit/Resources/User/Profile.cs
+++ SharpFit/Resources/User/Profile.cs
@@ -219,56 +219,7 @@ namespace SharpFit.Resources.User
 
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public void GetUserInfo()
-        {
-            RestRequest request;
-            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
-            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
-            request = new RestRequest("/user/-/profile.json");
-            client.ExecuteAsync(request, response =>
-            {
-                FitBitUser p = new FitBitUser();
-                p = JsonConvert.DeserializeObject<FitBitUser>(response.Content);
-                NotifyProfileComplete(p);
-            });
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public delegate void GetProfileHandler(object sender, ProfileEventArgs e);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public event GetProfileHandler GetProfileCompleted;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="param"></param>
-        public void UpdateUserInfo(Dictionary<string, string> param)
-        {
-            RestRequest request;
-            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
-            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
-            request = new RestRequest("/user/-/profile.json", Method.POST);
-            foreach (KeyValuePair<string, string> p in param)
-            {
-                request.AddParameter(p.Key, p.Value);
-            }
-            client.ExecuteAsync(request, response =>
-                {
-                    FitBitUser prof = new FitBitUser();
-                    prof = JsonConvert.DeserializeObject<FitBitUser>(response.Content);
-                    NotifyProfileComplete(prof);
-                });
-        } 
+         
         #endregion
 
         #region INotifyPropertyChanged Members
@@ -292,17 +243,7 @@ namespace SharpFit.Resources.User
 
         #endregion
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="profile"></param>
-        private void NotifyProfileComplete(FitBitUser profile)
-        {
-            if (GetProfileCompleted != null)
-            {
-                GetProfileCompleted(this, new ProfileEventArgs(profile));
-            }
-        } 
+        
     }
 
     /// <summary>
