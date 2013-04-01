2a0b2e6677855474e32eab406f3ae024b4a5e00e
Lucas Brendel <lucasbrendel@gmail.com>
2013-03-31 22:18:21 -0500

    Added blood pressure functions

--- SharpFit/Resources/BloodPressureInfo/FitBitBloodPressure.cs
+++ SharpFit/Resources/BloodPressureInfo/FitBitBloodPressure.cs
@@ -1,10 +1,12 @@
-﻿// JSON C# Class Generator
-// http://at-my-window.blogspot.com/?page=json-class-generator
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using SharpFit.OAuth;
+using SharpFit.Events;
+using SharpFit.Exceptions;
 
 namespace SharpFit.Resources.BloodPressureInfo
 {
@@ -24,5 +26,90 @@ namespace SharpFit.Resources.BloodPressureInfo
         /// </summary>
         [JsonProperty("bp")]
         public IList<Bp> Bp { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FitBitBloodPressure()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="avg"></param>
+        /// <param name="pressure"></param>
+        public FitBitBloodPressure(Average avg, Bp pressure)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        public void GetBloodPressure(DateTime date)
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest(String.Format("/1/user/-/bp/date/{0}-{1}-{2}.json", date.Year, date.Month, date.Day), Method.GET);
+            request.AddHeader("Accept-Language", "en_US");
+            client.ExecuteAsync(request, response =>
+                {
+                    FitBitBloodPressure bloodPressure = new FitBitBloodPressure();
+                    bloodPressure = JsonConvert.DeserializeObject<FitBitBloodPressure>(response.Content);
+
+                });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameters"></param>
+        public void LogBloodPressure(Dictionary<string, string> parameters)
+        {
+            if (parameters.ContainsKey("systolic") && parameters.ContainsKey("diastolic") && parameters.ContainsKey("date"))
+            {
+
+            }
+            else
+            {
+                throw new MissingParameterException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Systolic"></param>
+        /// <param name="Diastolic"></param>
+        /// <param name="date"></param>
+        public void LogBloodPressure(int Systolic, int Diastolic, DateTime date)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="systolic"></param>
+        /// <param name="diastolic"></param>
+        /// <param name="date"></param>
+        /// <param name="time">OPTIONAL parameter for the time of measurement</param>
+        public void LogBloodPressure(int systolic, int diastolic, DateTime date, DateTime time)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logID"></param>
+        public void DeleteBloodPressure(int logID)
+        {
+
+        }
     }
 }
---
+++ SharpFit/Resources/Device/Devices.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using SharpFit.OAuth;
+
+namespace SharpFit.Resources.Device
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Devices : INotifyPropertyChanged
+    {
+        private string _battery;
+        private string _id;
+        private DateTime _lastSyncTime;
+        private DeviceType _type;
+        private DeviceVersions _deviceVersion;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName="battery")]
+        public string Battery
+        {
+            get { return _battery; }
+            set
+            {
+                if (value != _battery)
+                {
+                    _battery = value;
+                    NotifyChanged("Battery");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName="id")]
+        public string ID
+        {
+            get { return _id; }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyChanged("ID");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName="lastSyncTime")]
+        public DateTime LastSyncTime
+        {
+            get { return _lastSyncTime; }
+            set
+            {
+                if (value != _lastSyncTime)
+                {
+                    _lastSyncTime = value;
+                    NotifyChanged("LastSyncTime");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName="type")]
+        public DeviceType Type
+        {
+            get { return _type; }
+            set
+            {
+                if (value != _type)
+                {
+                    _type = value;
+                    NotifyChanged("Type");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName="deviceVersion")]
+        public DeviceVersions DeviceVersion
+        {
+            get { return _deviceVersion; }
+            set
+            {
+                if (value != _deviceVersion)
+                {
+                    _deviceVersion = value;
+                    NotifyChanged("DeviceVersion");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum DeviceVersions
+        {
+            Classic,
+            Ultra,
+            Aria
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum DeviceType
+        {
+            TRACKER,
+            SCALE
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Devices()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void GetDevices()
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest("/user/-/devices.json");
+            client.ExecuteAsync(request, response =>
+                {
+                    IList<Devices> d = new List<Devices>();
+                    d = JsonConvert.DeserializeObject<List<Devices>>(response.Content);
+                    NotifyDownloadComplete(d);
+                });
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public delegate void DeviceDownloadCompleteEventHandler(object sender, DeviceDownloadEventArgs e);
+
+        public event DeviceDownloadCompleteEventHandler DevicesDownloaded;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="property"></param>
+        public void NotifyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="devices"></param>
+        public void NotifyDownloadComplete(IList<Devices> devices)
+        {
+            if (DevicesDownloaded != null)
+            {
+                DevicesDownloaded(this, new DeviceDownloadEventArgs(devices));
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DeviceDownloadEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public IList<Devices> DeviceList
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deviceList"></param>
+        public DeviceDownloadEventArgs(IList<Devices> deviceList)
+        {
+            DeviceList = deviceList;
+        }
+    }
+}
