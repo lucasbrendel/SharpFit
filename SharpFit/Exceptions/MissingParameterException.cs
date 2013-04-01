ff8eb254aefa0cb94f26656008d6905a5c92c465
Lucas Brendel <lucasbrendel@gmail.com>
2013-03-31 22:18:41 -0500

    Added exception class for missing parameter

--- SharpFit/Exceptions/MissingParameterException.cs
+++ SharpFit/Exceptions/MissingParameterException.cs
@@ -5,7 +5,31 @@ using System.Text;
 
 namespace SharpFit.Exceptions
 {
-    class MissingParameterException
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MissingParameterException : Exception
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Parameter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MissingParameterException(string parameter) : base()
+        {
+            this.Parameter = parameter;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        public MissingParameterException(string parameter, Exception ex) : base(parameter, ex)
+        {
+            this.Parameter = parameter;
+        }
     }
 }
