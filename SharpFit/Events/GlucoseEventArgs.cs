0543858bd08734d5b088248ae14cdbcbd2e24949
Lucas Brendel <lucasbrendel@gmail.com>
2013-04-09 06:36:12 -0500

    Get Glucose
    users can now get their glucose readings

---
+++ SharpFit/Events/GlucoseEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpFit.Resources.GlucoseInfo;
+
+namespace SharpFit.Events
+{
+    public class GlucoseGetEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public FitBitGlucose Glucose;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="glucose"></param>
+        public GlucoseGetEventArgs(FitBitGlucose glucose)
+        {
+            this.Glucose = glucose;
+        }
+    }
+}
--- SharpFit/Resources/GlucoseInfo/FitBitGlucose.cs
+++ SharpFit/Resources/GlucoseInfo/FitBitGlucose.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SharpFit.Events;
+using RestSharp;
+using RestSharp.Authenticators;
+using SharpFit.OAuth;
 
 namespace SharpFit.Resources.GlucoseInfo
 {
@@ -13,6 +17,9 @@ namespace SharpFit.Resources.GlucoseInfo
     /// </summary>
     public class FitBitGlucose
     {
+        public delegate void GlucoseGetEventHandler(object sender, GlucoseGetEventArgs e);
+
+        public event GlucoseGetEventHandler GlucoseReceived;
         /// <summary>
         /// 
         /// </summary>
@@ -24,5 +31,36 @@ namespace SharpFit.Resources.GlucoseInfo
         /// </summary>
         [JsonProperty("hba1c")]
         public double Hba1c { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        public void GetGlucose(DateTime date)
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest(String.Format("/user/-/glucose/date/{0}-{1}-{2}.json", date.Year, date.Month, date.Day), Method.GET);
+            request.AddHeader("Accept-Language", "en_US");
+            client.ExecuteAsync(request, response =>
+            {
+                FitBitGlucose glucose = new FitBitGlucose();
+                glucose = JsonConvert.DeserializeObject<FitBitGlucose>(response.Content);
+                NotifyReceived(glucose);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="glucose"></param>
+        private void NotifyReceived(FitBitGlucose glucose)
+        {
+            if (GlucoseReceived != null)
+            {
+                GlucoseReceived(this, new GlucoseGetEventArgs(glucose));
+            }
+        }
     }
 }
