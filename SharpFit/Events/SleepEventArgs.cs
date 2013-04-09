a95c873480237bb74f72ff5a28afcb17f35662b3
Lucas Brendel <lucasbrendel@gmail.com>
2013-04-09 06:49:40 -0500

    Added Get Sleep
    get users sleep information

---
+++ SharpFit/Events/SleepEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpFit.Resources.SleepInfo;
+
+namespace SharpFit.Events
+{
+    public class SleepGetEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public FitBitSleep Sleep;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sleep"></param>
+        public SleepGetEventArgs(FitBitSleep sleep)
+        {
+            this.Sleep = sleep;
+        }
+    }
+}
--- SharpFit/Resources/SleepInfo/FitBitSleep.cs
+++ SharpFit/Resources/SleepInfo/FitBitSleep.cs
@@ -5,14 +5,18 @@ using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using SharpFit.OAuth;
+using SharpFit.Events;
 
 namespace SharpFit.Resources.SleepInfo
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class FitBitSleep
     {
+        public delegate void SleepGetEventHandler(object sender, SleepGetEventArgs e);
+
+        public event SleepGetEventHandler SleepReceived;
         /// <summary>
         /// 
         /// </summary>
@@ -24,5 +28,36 @@ namespace SharpFit.Resources.SleepInfo
         /// </summary>
         [JsonProperty("summary")]
         public Summary Summary { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        public void GetSleep(DateTime date)
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest(String.Format("/user/-/glucose/date/{0}-{1}-{2}.json", date.Year, date.Month, date.Day), Method.GET);
+            request.AddHeader("Accept-Language", "en_US");
+            client.ExecuteAsync(request, response =>
+            {
+                FitBitSleep sleep = new FitBitSleep();
+                sleep = JsonConvert.DeserializeObject<FitBitSleep>(response.Content);
+                NotifyReceived(sleep);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sleep"></param>
+        private void NotifyReceived(FitBitSleep sleep)
+        {
+            if (SleepReceived != null)
+            {
+                SleepReceived(this, new SleepGetEventArgs(sleep));
+            }
+        }
     }
 }
