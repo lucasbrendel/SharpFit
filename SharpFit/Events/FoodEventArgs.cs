54bee38795afaed9b502e7064baa7708ad959c34
Lucas Brendel <lucasbrendel@gmail.com>
2013-04-14 15:56:32 -0500

    Get foods

---
+++ SharpFit/Events/FoodEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpFit.Resources.FoodInfo;
+
+namespace SharpFit.Events
+{
+    public class FoodGetEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public FitBitFood Foods;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="foods"></param>
+        public FoodGetEventArgs(FitBitFood foods)
+        {
+            this.Foods = foods;
+        }
+    }
+}
--- SharpFit/Resources/FoodInfo/FitBitFood.cs
+++ SharpFit/Resources/FoodInfo/FitBitFood.cs
@@ -1,10 +1,11 @@
-﻿// JSON C# Class Generator
-// http://at-my-window.blogspot.com/?page=json-class-generator
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using SharpFit.Events;
+using SharpFit.OAuth;
 
 namespace SharpFit.Resources.FoodInfo
 {
@@ -13,6 +14,10 @@ namespace SharpFit.Resources.FoodInfo
     /// </summary>
     public class FitBitFood
     {
+        public delegate void FoodReceivedEventHandler(object sender, FoodGetEventArgs e);
+
+        public event FoodReceivedEventHandler FoodReceived;
+
         /// <summary>
         /// 
         /// </summary>
@@ -30,5 +35,36 @@ namespace SharpFit.Resources.FoodInfo
         /// </summary>
         [JsonProperty("goals")]
         public Goals Goals { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        public void GetFoods(DateTime date)
+        {
+            RestRequest request;
+            var client = new RestClient(OAuthCredentials.APIAccessStringWithVersion);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(OAuthCredentials.ConsumerKey, OAuthCredentials.ConsumerSecret, OAuthCredentials.AccessToken, OAuthCredentials.AccessTokenSecret);
+            request = new RestRequest(String.Format("/user/-/foods/log/date/{0}-{1}-{2}.json", date.Year, date.Month, date.Day), Method.GET);
+            request.AddHeader("Accept-Language", "en_US");
+            client.ExecuteAsync(request, response =>
+            {
+                FitBitFood food = new FitBitFood();
+                food = JsonConvert.DeserializeObject<FitBitFood>(response.Content);
+                NotifyReceived(food);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="food"></param>
+        private void NotifyReceived(FitBitFood food)
+        {
+            if (FoodReceived != null)
+            {
+                FoodReceived(this, new FoodGetEventArgs(food));
+            }
+        }
     }
 }
